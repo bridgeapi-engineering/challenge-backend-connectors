6367b3eae5f6cd799e222e4d27134cdbbd310c77
Aurel Stocheci <stocheci.aurel@gmail.com>
2020-12-07 12:38:23 +0200

    review

--- review/fileToReview.js
+++ review/fileToReview.js
@@ -1,4 +1,4 @@
-
+// review: const should be used here
 var domain = "bank.local.fr"
 
 /**
@@ -11,9 +11,13 @@ var domain = "bank.local.fr"
  * @param {Object} previousTransactions Previous page of transactions (To ckeck for dupes)
  * @return {Object} All transactions available on the page
  */
+// review: function is not exported
+// default param should be last
+// the number of param is high, maybe a param like options = {} is a solution
 async function fetchTransactions(fromDate, authorization, jws = null, id, page, previousTransactions) {
 	console.log(`--- Fetch Trasactions page n°${page} ---`);
 	try {
+	  // block 1
     var headers = {"Authorisation":  authorization }
 
     if (jws) {
@@ -30,12 +34,27 @@ async function fetchTransactions(fromDate, authorization, jws = null, id, page, 
         "Accept": "application/json",
       }
     }
+    // end block 1
 
+    // review: block1 can be simplified like this
+    // const headers = {
+    //   "Authorisation": authorization,
+    //   "Content-type": "application/json",
+    //   "Accept": "application/json",
+    // }
+    // if (jws) {
+    //   headers.jws = jws;
+    // }
+
+    // review: doRequest id not imported
+    // review: domain + '/accounts/'+ id + '/transactions?' + `page=${page}` is more readable if string interpolation is used
+    // `${domain}/accounts/${id}/transactions?page=${page}`
 	  var {code, response } = await doRequest('GET',
       domain + '/accounts/'+ id + '/transactions?' + `page=${page}`,
       headers);
 
 
+    // review: === should be used instead == to avoid unexpected type coercion
 		if (response && code == 200 && response.data) {
       if (response.data.meta) {
         if (response.data.meta.hasPageSuivante) {
@@ -46,6 +65,7 @@ async function fetchTransactions(fromDate, authorization, jws = null, id, page, 
           } else {
             // if we have mouvements
             if (mouvements) {
+              // review: assertTransactions is not imported
               if (assertTransactions(mouvements)) {
                 return [];
               } else {
@@ -54,6 +74,8 @@ async function fetchTransactions(fromDate, authorization, jws = null, id, page, 
             } else {
               throw new Error("Empty list of transactions ! " + JSON.stringify(previousTransactions));
             }
+            // review: fetchTransactions it's an async function, it should be await
+            // nextPagesTransactions will not be an array, it will be a Promise
             let nextPagesTransactions = fetchTransactions(fromDate, authorization, (jws || null), id, page + 1, mouvements);
             response.data.Mouvements = mouvements.concat(nextPagesTransactions);
           }
@@ -64,9 +86,11 @@ async function fetchTransactions(fromDate, authorization, jws = null, id, page, 
 
     return [];
 	} catch (err) {
+	  // review: CustomError is not imported
 		throw new CustomError({
       function: 'fetchTransactions',
 			statusCode: 'CRASH',
+      // review: err should be used here
 			rawError: e,
 		});
 	}
