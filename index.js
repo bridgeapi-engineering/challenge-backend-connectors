b1b5cad6a3a2fd873ab63e7bb68c2e5a6371d724
sumairaJ <javaid.sumaira@gmail.com>
2022-07-30 18:29:10 +0200

    get accounts

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ require('dotenv').config();
 const axios = require('axios');
 
 const getRefreshToken = async () => {
-    const basicAuth = {
-        username: process.env.CLIENTID,
-        password: process.env.CLIENTSECRET
-    };
-
-    const data = {
-        user: process.env.LOGIN,
-        password: process.env.PASSWORD
-    }
+    const basicAuth = {username: process.env.CLIENTID, password: process.env.CLIENTSECRET};
+    const data = {user: process.env.LOGIN, password: process.env.PASSWORD}
 
     const res = await axios.post('http://localhost:3000/login', data, {auth: basicAuth});
     console.log(`statusCode RefreshToken: ${res.status}`);
@@ -24,12 +17,18 @@ const getAccessToken = async () => {
     parameters.append('refresh_token', refreshToken);
     const headers =  {"Content-Type": "application/x-www-form-urlencoded"}
 
-    const res = await axios.post(
-        'http://localhost:3000/token',
-        parameters,
-        {headers: headers});
+    const res = await axios.post('http://localhost:3000/token', parameters, {headers: headers});
     console.log(`statusCode AccessToken: ${res.status}`);
     return res.data.access_token;
 }
 
-getAccessToken().then(token => console.log(token)).catch(error => console.log(error.message))
+const getAccounts = async () => {
+    const accessToken = await getAccessToken();
+    const config = {headers: {'Authorization': 'Bearer ' + accessToken}}
+
+    const res = await axios.get('http://localhost:3000/accounts', config);
+    console.log(`statusCode Accounts: ${res.status}`);
+    return res.data.account;
+}
+
+getAccounts().then(account => console.log(account)).catch(error => console.log(error.message))
